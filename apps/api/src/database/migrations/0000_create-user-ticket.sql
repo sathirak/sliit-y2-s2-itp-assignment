7487294273bbc0b3b293373e7d7205c0805ed270
sathirak <145209193+sathirak@users.noreply.github.com>
2025-08-24 15:32:44 +0530

    fix: migration issue

--- apps/api/src/database/migrations/0000_create-user-ticket.sql
+++ apps/api/src/database/migrations/0000_create-user-ticket.sql
@@ -1,3 +1,12 @@
+CREATE TABLE "tickets" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" text NOT NULL,
+	"description" text NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"deleted" boolean DEFAULT false NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "tickets" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
 CREATE TABLE "users" (
 	"user_id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" text NOT NULL,
