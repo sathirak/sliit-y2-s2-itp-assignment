b8af2edff1ff286ca0e140f0bb7e56b195fcda91
sathirak <145209193+sathirak@users.noreply.github.com>
2025-08-29 09:29:43 +0530

    Feat/image upload (#42)
    * feat: postgress docker compose file added
    
    * feat: product management CRUD added
    
    * feat: health module and better database handling added
    
    * feat: health module and better database handling added
    
    * chore: remove .vscode from git tracking
    
    * feat: product filtering and pagination added
    
    * feat: upload module added
    
    * feat: upload module added
    
    * feat: upload module added
    
    * fix: drizzle
    
    ---------
    
    Co-authored-by: thilu17 <thilu17@users.noreply.github.com>

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { Logger as AppLogger } from 'nestjs-pino';
 import { AppModule } from './app.module';
