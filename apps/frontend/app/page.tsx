ba9bb7ced060cae27d42199d5b09af67d568459a
KavishkaWijewardhana <kaviwijewardhana@gmail.com>
2025-09-20 09:50:23 +0530

    feat: add ChatBubble component to Home page

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import { NewArrivals } from "../modules/common/components/NewArrivals";
+import ChatBubble from "@/modules/chatbot/components/Chatbubble";
 
 export default function Home() {
   return (
     <>
       <NewArrivals />
+      <ChatBubble />
     </> 
+
   );
 }
